c146bd04cb5029e429008bb3a5a96666c008e397
Saykora <rumiancevpro@gmail.com>
2025-04-22 14:10:00 +0300

    Створив просір імен

--- ConsoleApp1_Lab_1-2/Program.cs
+++ ConsoleApp1_Lab_1-2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp1_Lab_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створення об'єкта автомобіля
+            Car myCar = new Car("Toyota", "Camry", 2020, "Червоний", 15000);
+
+            // Виведення інформації про автомобіль
+            myCar.DisplayInfo();
+
+            // Оновлення пробігу
+            myCar.UpdateMileage(20000);
+
+            // Виведення оновленої інформації про автомобіль
+            myCar.DisplayInfo();
+        }
+    }
+}
